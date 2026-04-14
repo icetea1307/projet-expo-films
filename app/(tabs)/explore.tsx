cfe812ccea902e5683ba136a7a5528c825d9b0ba
aghugo <hugo.aguer@ynov.com>
2026-04-14 11:02:07 +0200

    3

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,17 +1,70 @@
-import { View, TextInput, StyleSheet } from 'react-native';
+import { useState } from 'react';
+import {
+  View,
+  TextInput,
+  StyleSheet,
+  FlatList,
+  Text,
+  Image,
+} from 'react-native';
 import Ionicons from '@expo/vector-icons/Ionicons';
 
+const API_KEY = "f3d3ad9ea60a687311952816106b86a3";
+
 export default function ExploreScreen() {
+  const [query, setQuery] = useState('');
+  const [movies, setMovies] = useState([]);
+
+  const searchMovies = async (text: string) => {
+    setQuery(text);
+
+    if (text.length < 2) {
+      setMovies([]);
+      return;
+    }
+
+    const res = await fetch(
+      `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${text}`
+    );
+
+    const data = await res.json();
+    setMovies(data.results || []);
+  };
+
   return (
     <View style={styles.container}>
+      {/* BARRE */}
       <View style={styles.searchBar}>
         <Ionicons name="search" size={18} color="#8E8E93" />
         <TextInput
           style={styles.input}
           placeholder="Search"
           placeholderTextColor="#8E8E93"
+          value={query}
+          onChangeText={searchMovies}
         />
       </View>
+
+      {/* RESULTATS */}
+      {query.length >= 2 && movies.length === 0 && (
+        <Text style={styles.noResult}>Aucun résultat</Text>
+      )}
+
+      <FlatList
+        data={movies}
+        keyExtractor={(item: any) => item.id.toString()}
+        renderItem={({ item }: any) => (
+          <View style={styles.card}>
+            <Image
+              source={{
+                uri: `https://image.tmdb.org/t/p/w500${item.poster_path}`,
+              }}
+              style={styles.image}
+            />
+            <Text style={styles.title}>{item.title}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 }
@@ -30,11 +83,30 @@ const styles = StyleSheet.create({
     paddingHorizontal: 14,
     flexDirection: 'row',
     alignItems: 'center',
+    marginBottom: 10,
   },
   input: {
     flex: 1,
     color: 'white',
     marginLeft: 10,
     fontSize: 15,
+  },
+  noResult: {
+    color: 'white',
+    textAlign: 'center',
+    marginTop: 20,
+  },
+  card: {
+    marginVertical: 10,
+    alignItems: 'center',
+  },
+  image: {
+    width: 120,
+    height: 180,
+    borderRadius: 10,
+  },
+  title: {
+    color: 'white',
+    marginTop: 5,
   },
 });
